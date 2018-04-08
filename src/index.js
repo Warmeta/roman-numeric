1fb1f83e0e65d779bcde73fc57153b6831b76ab9
warmeta <warmeta@gmail.com>
2018-04-08 03:47:08 +0100

    type control on server for query param

--- src/index.js
+++ src/index.js
@@ -6,5 +6,5 @@ http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   var q = url.parse(req.url, true).query;
   var roman = romanNumeric.transformToRoman(q.number);
-  res.end(`The roman number in query parameter is ${roman}`);
+  Number.isInteger(parseInt(q.number)) ? res.end(`The roman number in query parameter is ${roman}`) : res.end(`That is not a number... don't troll`);
 }).listen(8080);
