31a80f31a2e12cb4a5f90ac41cf79381693a2231
warmeta <warmeta@gmail.com>
2018-04-08 03:47:52 +0100

    test fix

--- test/indexTest.spec.js
+++ test/indexTest.spec.js
@@ -1,11 +1,11 @@
-const RomanNumber = require('../src');
+var romanNumeric = require('../src/romanNumeric');
 const assert = require('assert');
 
 suite('RomanNumber', () => {
   test('1 to 10', () => {
     const expectedResult = {1:'I', 2:'II', 3:'III', 4:'IV', 5:'V', 6:'VI', 7:'VII', 8:'VIII', 9:'IX', 10:'X'};
     for(i = 1; i <= 10; i++) {
-      assert.equal(RomanNumber(i), expectedResult[i]);
+      assert.equal(romanNumeric.transformToRoman(i), expectedResult[i]);
     };
   });
 });
