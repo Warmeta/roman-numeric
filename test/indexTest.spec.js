5cfa010c1e3c99b42fea771718ca14eaa64b1b94
warmeta <warmeta@gmail.com>
2018-04-08 02:56:35 +0100

    tests the first 10 to roman

--- test/indexTest.spec.js
+++ test/indexTest.spec.js
@@ -2,11 +2,10 @@ const RomanNumber = require('../src');
 const assert = require('assert');
 
 suite('RomanNumber', () => {
-    test('1 => I', () => {
-        assert.equal(RomanNumber(1),'I');
-    });
-
-    test('2 => II', () => {
-        assert.equal(RomanNumber(2),'II');
-    });
+  test('1 to 10', () => {
+    const expectedResult = {1:'I', 2:'II', 3:'III', 4:'IV', 5:'V', 6:'VI', 7:'VII', 8:'VIII', 9:'IX', 10:'X'};
+    for(i = 1; i <= 10; i++) {
+      assert.equal(RomanNumber(i), expectedResult[i]);
+    };
+  });
 });
